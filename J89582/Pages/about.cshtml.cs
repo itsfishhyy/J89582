eb3f20e0b516b50b1fa22395906a6168bb567ce1
Nabillah <nabillah315@gmail.com>
2023-04-14 00:11:16 +0800

    Add project files.

---
+++ J89582/Pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace burgerorzo.Pages
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
