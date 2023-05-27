2ac1ad98cd309d918fa7361b1c6ed8efa49cbdda
Nabillah/J89582 <124653122+itsfishhyy@users.noreply.github.com>
2023-05-28 02:52:47 +0800

    Update J89582Context.cs

--- J89582/Data/J89582Context.cs
+++ J89582/Data/J89582Context.cs
@@ -1,13 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using J89582.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace J89582.Data
 {
-    public class J89582Context : DbContext
+    public class J89582Context : IdentityDbContext<ApplicationUser>
     {
         public J89582Context(DbContextOptions<J89582Context> options)
             : base(options)
@@ -15,5 +16,6 @@ namespace J89582.Data
         }
 
         public DbSet<J89582.Model.Menu> Menu { get; set; } = default!;
+
     }
 }
