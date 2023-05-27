d02ab487d8c4beff8b962fade4abdbb7288f3cc9
Nabillah/J89582 <124653122+itsfishhyy@users.noreply.github.com>
2023-05-28 02:52:14 +0800

    Update Program.cs

--- J89582/Program.cs
+++ J89582/Program.cs
@@ -39,11 +39,10 @@ builder.Services.AddRazorPages(options =>
 
 var app = builder.Build();
 
-//using (var scope = app.Services.CreateScope)
-//{
-//    var services = scope.ServiceProvider;
-//    SeedData.Initialize(services);
-//}
+/*using (var scope = app.Services.CreateScope)
+{
+    var services = scope.ServiceProvider;
+}*/
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -51,6 +50,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//else
+//{
+//    app.UseDeveloperExceptionPage();
+//    app.UseMigrationEndPoint();
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
